70daa5637faa9b27da0528f6712b73cda5bee536
Jeff-vinicius <jeffersonshady10@gmail.com>
2022-08-07 20:49:13 -0300

    created integration test use case CreateCategory

---
+++ tests/Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,48 @@
+﻿using Codeflix.Catalog.Infra.Data.EF;
+using Codeflix.Catalog.Infra.Data.EF.Repositories;
+using FluentAssertions;
+using Xunit;
+using ApplicationUseCases = Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+
+namespace Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.CreateCategory
+{
+    [Collection(nameof(CreateCategoryTestFixture))]
+    public class CreateCategoryTest
+    {
+        private readonly CreateCategoryTestFixture _fixture;
+
+        public CreateCategoryTest(CreateCategoryTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = nameof(CreateCategory))]
+        [Trait("Integration/Application", "CreateCategory - Use Cases")]
+        public async void CreateCategory()
+        {
+            var dbContext = _fixture.CreateDbContext();
+            var repository = new CategoryRepository(dbContext);
+            var unitOfWork = new UnitOfWork(dbContext);
+
+            var useCase = new ApplicationUseCases.CreateCategory(unitOfWork, repository);
+
+            var input = _fixture.GetInput();
+
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+            var dbCategory = await dbContext.Categories.FindAsync(output.Id);
+            dbCategory.Should().NotBeNull();
+            dbCategory!.Name.Should().Be(input.Name);
+            dbCategory.Description.Should().Be(input.Description);
+            dbCategory.IsActive.Should().Be(input.IsActive);
+            dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+
+            output.Id.Should().NotBeEmpty();
+            output.Should().NotBeNull();
+            output.Name.Should().Be(input.Name);
+            output.Description.Should().Be(input.Description);
+            output.IsActive.Should().Be(input.IsActive);
+            output.CreatedAt.Should().NotBeSameDateAs(default);
+        }
+    }
+}
