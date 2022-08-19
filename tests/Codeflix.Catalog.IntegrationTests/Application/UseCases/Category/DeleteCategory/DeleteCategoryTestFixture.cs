1d445e1fa3ff1f41167e60a29e66c2947a8bd0a8
Jeff-vinicius <jeffersonshady10@gmail.com>
2022-08-19 20:51:11 -0300

    created integration test use case DeleteCategory

---
+++ tests/Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.DeleteCategory
+{
+    [CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+    public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture>
+    { }
+
+    public class DeleteCategoryTestFixture : CategoryUseCasesBaseFixture
+    {
+    }
+
+}
