3a1101272ac91bb7097cc7d3db9aaf118be5a7d2
Jeff-vinicius <jeffersonshady10@gmail.com>
2022-08-23 20:45:25 -0300

    created project API and first test EndToEnd

---
+++ tests/Codeflix.Catalog.EndToEndTests/Api/Category/Common/CategoryBaseFixture.cs
@@ -0,0 +1,45 @@
+﻿using Codeflix.Catalog.EndToEndTests.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeflix.Catalog.EndToEndTests.Api.Category.Common
+{
+    public class CategoryBaseFixture : BaseFixture
+    {
+        public CategoryPersistence Persistence;
+
+        public CategoryBaseFixture() : base()
+        {
+            Persistence = new CategoryPersistence(CreateDbContext());
+        }
+
+        public string GetValidCategoryName()
+        {
+            var categoryName = string.Empty;
+
+            while (categoryName.Length < 3)
+                categoryName = Faker.Commerce.Categories(1)[0];
+
+            if (categoryName.Length > 255)
+                categoryName = categoryName[..255];
+
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription = Faker.Commerce.ProductDescription();
+
+            if (categoryDescription.Length > 10_000)
+                categoryDescription = categoryDescription[..10_000];
+
+            return categoryDescription;
+        }
+
+        public bool GetRandomBoolean()
+            => (new Random()).NextDouble() < 0.5;
+    }
+}
